35eed542ee0ef4b13406b8fd22fc5ad9f9a1cd80
giwan <rman@neople.co.kr>
2022-02-20 21:13:53 +0900

    html init template

--- main.go
+++ main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/pkw5240/blockchain/blockchain"
 )
 
 const port string = ":4000"
 
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
 func home(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(rw, "hello from home!")
+	tmpl := template.Must(template.ParseFiles("templates/home.html"))
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+	tmpl.Execute(rw, data)
+	//fmt.Fprintf(rw, "hello from home!")
 }
 
 func main() {
