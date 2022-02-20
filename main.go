933910d67ee69cbaf8d4068f9517b091eacc8b09
giwan <rman@neople.co.kr>
2022-02-20 20:41:08 +0900

    4.5

--- main.go
+++ main.go
@@ -8,5 +8,15 @@ import (
 
 func main() {
 	chain := blockchain.GetBlockchain()
+	chain.AddBlock("second")
+	chain.AddBlock("third")
+
 	fmt.Println(chain)
+
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("data : %s \n", block.Data)
+		fmt.Printf("hash : %x \n", block.Hash)
+		fmt.Printf("PrevHash : %x \n", block.PrevHash)
+	}
+
 }
